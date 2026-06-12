aa591f33b141c3052227642193b1e462147d1c6e
Eric Busboom <eric.busboom@jointheleague.org>
2026-06-11 20:10:25 -0700

    chore: bump version to 0.20260611.10

--- source/types/Protocol.h
+++ source/types/Protocol.h
@@ -15,7 +15,7 @@ constexpr const char* PROTO_TAG_ID  = "ID";
 // Protocol version and firmware version string
 // ---------------------------------------------------------------------------
 constexpr int         PROTO_VERSION    = 2;
-constexpr const char* FIRMWARE_VERSION = "0.20260611.8";
+constexpr const char* FIRMWARE_VERSION = "0.20260611.10";
 
 using ReplyFn = void(*)(const char* msg, void* ctx);
 
